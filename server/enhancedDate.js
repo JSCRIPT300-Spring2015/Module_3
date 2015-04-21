f92d22a3844806e14265611a28a0c9eb1a4dad8f
9shadesdesign <brianforcum@gmail.com>
2015-04-20 20:14:59 -0700

    testing server

--- server/enhancedDate.js
+++ server/enhancedDate.js
@@ -1,1 +1,82 @@
 // export this as a Node module using the enhancedDate.js code from Module_2
+
+
+  
+ 'use strict';
+  
+  var theDate = {
+    time: new Date(),
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 
+           'Friday', 'Saturday'],
+    months: ['January', 'February', 'March', 'April', 'May', 'June',
+             'July', 'August', 'September', 'October', 'November',
+             'December'],
+    
+  };
+  
+ 
+  
+    var setDate = function (d) {
+      
+      if (!(d instanceof Date)) {
+          theDate.time = new Date();
+      } else {
+        theDate.time = d;
+      }
+          return theDate.time;
+    };
+    
+    var getDate = function (param) {
+      var gDate;
+      if (param === true) {
+        gDate = theDate.time;
+      }  else {
+        gDate = theDate.time.getTime();
+      }
+      return gDate;
+     };
+    
+    var getDayName = function () {
+      var dayI = theDate.time.getDay();
+      var day = theDate.days[dayI];
+      return day;
+     };
+    
+    var getMonthName = function () {
+      var monthI = theDate.time.getMonth();
+      var month = theDate.months[monthI];
+      return month;
+     };
+    
+    var isFuture = function () {
+      var now = new Date();
+      if (theDate.time.getTime() > now) {
+        return true;
+      }  else {
+        return false;
+      }
+     };
+    
+    var isToday = function isToday() {
+      var today = new Date();
+      if (today.getYear() === theDate.time.getYear() && 
+          today.getMonth() === theDate.time.getMonth() &&
+          today.getDay() === theDate.time.getDay()) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+      
+module.exports.setDate = setDate;
+module.exports.getDate = getDate;
+module.exports.getDayName = getDayName;
+module.exports.getMonthName = getMonthName;
+module.exports.isFuture = isFuture;
+module.exports.isToday = isToday;
+  
+    
+  
+
+
+    
--- server/server.js
+++ server/server.js
@@ -11,3 +11,20 @@
 // iterating through that list, display the string you built up.
 
 // Remember that the response is a stream object that must be closed.
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE "html">");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
+
+server.listen(80);
+console.log("Server is listening");
