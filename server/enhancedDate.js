8f7cf75fe2e64bcc494da6e7272057d7f84c39a0
Ted <tedmmc@uw.edu>
2015-04-21 15:48:54 -0700

    completely lost

--- server/enhancedDate.js
+++ server/enhancedDate.js
@@ -1,1 +1,87 @@
 // export this as a Node module using the enhancedDate.js code from Module_2
+
+// code from module_2 modified for node use
+var exports.enhancedDate = (function() {
+    'use strict';
+
+    //create object
+    var myObject = {};
+    
+    //create variables
+    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
+    myObject.setDate = function(passedDate) {
+        var now;
+
+        if (passedDate) {
+            if (passedDate instanceof Date) {
+                now = passedDate;
+            } else if (typeof (passedDate) === 'number') {
+                now = new Date(passedDate);
+            } else {
+                now = new Date();
+            }
+        }else {
+            //nothing passed in so get current datetime
+            now = new Date();
+        }
+
+        myObject.now = now;
+    };
+
+    myObject.getDate = function(wantObject) {
+        if (wantObject === true) {
+            return myObject.now;
+        }else {
+            return myObject.now.getTime();
+        }
+    };
+
+    myObject.getDayName = function() {
+        var dayNumber = myObject.now.getDay();
+        return daysOfWeek[dayNumber];
+    };
+
+    myObject.getMonthName = function() {
+        var monthNumber = myObject.now.getMonth();
+
+        return monthNames[monthNumber];
+    };
+
+    myObject.isFuture = function() {
+        var currentTime = new Date();
+
+        if (myObject.now > currentTime) {
+            return true;
+        }else {
+            return false;
+        }
+    };
+
+    myObject.isToday = function() {
+        var currentTime = new Date();
+        var currentYear = currentTime.getYear();
+        var currentMonth = currentTime.getMonth();
+        var currentDay = currentTime.getDay();
+
+        var objectTime = myObject.now;
+        var objectYear = objectTime.getYear();
+        var objectMonth = objectTime.getMonth();
+        var objectDay = objectTime.getDay();
+
+        if (objectYear === currentYear && objectMonth === currentMonth && objectDay === currentDay) {
+            return true;
+        }else {
+            return false;
+        }
+    };
+
+    //return myObject;
+    
+    return{
+       myObject.now: myObject.now; 
+    };
+    
+    
+})();
--- server/server.js
+++ server/server.js
@@ -11,3 +11,27 @@
 // iterating through that list, display the string you built up.
 
 // Remember that the response is a stream object that must be closed.
+
+
+/*
+var simple = require('./simplestever'); //even though same directory use ./ and leave off js extension
+
+// This will output 42
+console.log(simple.answer);
+*/
+
+
+var nodeDate = require('./enhancedDate'); //leave off file extension
+
+console.log(nodeDate.now);
+/*
+
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+*/
