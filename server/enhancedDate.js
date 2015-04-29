5e0f027a469c49de753615616fd3ae78b8c912c0
_Tyler Waite_ <waite.tyler@gmail.com>
2015-04-28 17:50:49 -0700

    Tyler Waite's Module 3 updates
    My updates to the Module 3 assignment.

--- server/enhancedDate.js
+++ server/enhancedDate.js
@@ -1,133 +1,90 @@
-var enhancedDate = (function (date) {
-// still maintains access to all globals
-  
-  var exports = module.exports = {};
+//Tyler Waite Module 3 enhanced date module version 1.0.0 4/28/2015
 
-//Stores the passed-in date.  
-  exports.setDate = function (date) {
-      var myDate = date;
-    
-//Do some type-checking to make sure you have a valid type.    
-      if(typeof myDate !== null)
-      {
-//Takes milliseconds after epoch or a date object. 
-        
-//Do some type-checking to make sure you have a valid type. 
-        if (typeof myDate == Date() || typeof myDate == "number"){
-          myDate = date;
-        } else {
-          myDate = false;
-        } 
-      }
-      else{
-        
-//If no value is passed, default to now.       
-        setDate = Date(year, month, day, hours, minutes, seconds, milliseconds);
-      }
+var enhancedDate = function () {
+//This is the enhancedDate code James wrote in class.
+  'use strict';
   
+  var myDate = null;
+  var dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  var monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  var dateSet = false;
+  
+function setDate(date) {
+  
+  if (date instanceof Date || typeof date === 'number') {
+    myDate = new Date(date);
+  } else {
+    myDate = new Date();
+  }
+  dateSet = true;
+}
+
+function getDate(returnObj) {
+  
+  if (!dateSet) {
+    setDate();
+  }
+  
+  if (returnObj) {
       return myDate;
-  };
+  } else {
+      return myDate.getTime();
+  }
+} 
 
-
-//Returns either milliseconds after epoch by default or a date object if true is passed as an argument.            
-  exports.getDate = setDate.getTime();
-
-//Returns the full day name as a string, e.g. "Monday", "Tuesday", etc.  
-  exports.getDayName = function () {
-      switch (setDate.getDay()) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-    }
-    
-    return;
-    
-    };
+function getDayName() {
+  if (!dateSet) {
+    setDate();
+  }
+  var day = myDate.getDay();
   
-//Determines the full month name as a string, e.g. "January", "February", etc.  
-  exports.getMonthName = function () {
-    switch (setDate.getMonth()) {
-        case 0:
-            day = "January";
-            break;
-        case 1:
-            day = "February";
-            break;
-        case 2:
-            day = "March";
-            break;
-        case 3:
-            day = "April";
-            break;
-        case 4:
-            day = "May";
-            break;
-        case 5:
-            day = "June";
-            break;
-        case 6:
-            day = "July";
-            break;
-        case 7:
-            day = "August";
-            break;
-        case 8:
-            day = "September";
-            break;
-        case 9:
-            day = "October";
-            break;
-        case 10:
-            day = "November";
-            break;
-        case 11:
-            day = "December";
-            break; 
-    }
-    return;
-  };
+  return dayNames[day];
+}  
   
-//Return boolean true if date is in the future (from when method is called)  
-  exports.isFuture = function () {
-    var today = new Date();
-    
-    if (setDate.Date() > today) {
+function getMonthNames(){
+  if (!dateSet) {
+    setDate();
+  }
+  
+  var month = myDate.getMonth();
+  
+  return monthNames [month];
+}
+
+function isToday() {
+  if (!dateSet) {
+    setDate();
+  }
+  
+  var today = new Date();
+  var month = today.getMonth();
+  var date = today.getDate();
+  var year = today.getYear();
+  
+  if (myDate.getMonth () === month && myDate.getDate() === date && myDate.getYear() === year) {
       return true;
-    } else {
+  } else {
       return false;
-    }
-  };
+  }
+}
   
-//Return boolean true if the stored date is "today".  
-  exports.isToday = function () {
-    var today = new Date();
-    
-    if (setDate.Date() === today) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-	
-}());
+function isFuture() {
+  if (!dateSet) {
+    setDate();
+  }
+  
+  return myDate.getTime() > Date.now();
+}  
+  
+//return {
+//  setDate: setDate,
+//  getDate: getDate,
+//  getDayName: getDayName,
+//  getMonthName: getMonthName,
+//  isToday: isToday,
+//  isFuture: isFuture
+//};
 
+};
 
-
+module.exports = enhancedDate;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
+//Tyler Waite Module 3 simple server version 1.0.0 4/28/2015
 // make this a simple http server that writes to the response stream object
-var http = require('http'); http.createServer(function (request, response) {     response.writeHead(200, { 'Content-Type': 'text/plain' });
-                                                            // the page should display the following message:
+var http = require('http');
 
-// Today is <day name>, <month name> <date>. Here are the available food trucks:
-                                                            
-    response.write('Today is ' + enhancedDate.getDayName +'Here are the available food trucks:');     response.end('Goodbye, class!'); }).listen(3000, function () {     console.log('listening on port 3000'); });
+var server = http.createServer();
+var dayName = enhancedDate.getDate.getDayName();
+var monthName = enhancedDate.getDate.getMonth();
+var dayNum = enhancedDate.getDate.getDate();
+var todaysTrucks = trucks.filterByDay();
 
+server.on('request', function(response){
+  
+  response.writeHead(200, {'Content-Type':'text/plain'});
+  response.write('Today is ' + dayName + monthName + dayNum);
+  response.write('The food trucks available are:' + foodTrucks.filterByDay(dayName));
+  
+});
 
-// list the food trucks returned by filterByDay, passing in the current day name.
-// e.g. filterByDay(day); where "day" is determined using the enhancedDate module
-// The list of trucks returned will be an array of food truck objects. Iterate 
-// through the list, building up a string of food truck names. Once you're done 
-// iterating through that list, display the string you built up.
-
-// Remember that the response is a stream object that must be closed.
+server.listen(3000, function () {
+    console.log('listening on port 3000');
+});
